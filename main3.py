d16fd024a7b95cae795690885e9d1c6dc8c8fe33
YanaArkhipova <yanaarkhipova1511@gmail.com>
2026-09-22 18:05:07 +0300

    main3.py

---
+++ main3.py
@@ -0,0 +1,7 @@
+n1= int(input(" Natural number 1: "))
+n2= int(input(" Natural number 2: "))
+n3= int(input(" Natural number 3: "))
+
+print("Tens 1: ", (n1 //10)%10 )
+print("Tens 2: ", (n2 //10)%10 )
+print("Tens 3: ", (n3 //10)%10 )
