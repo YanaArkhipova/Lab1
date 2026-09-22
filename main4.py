4bb6eda5b065857b13116e7861cb5ec9e797315b
YanaArkhipova <yanaarkhipova1511@gmail.com>
2026-09-22 18:05:43 +0300

    main4.py

---
+++ main4.py
@@ -0,0 +1,7 @@
+n1=int(input('Enter the number 1: '))
+n2=int(input('Enter the number 2: '))
+n3=int(input('Enter the number 3: '))
+
+print('Sum 1:', n1*(n1+1)//2)
+print('Sum 2:', n2*(n2+1)//2)
+print('Sum 3:', n3*(n3+1)//2)
