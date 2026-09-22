9c7c6cc88da6a1bca1c1f946b15ce64aa3bbe12e
YanaArkhipova <yanaarkhipova1511@gmail.com>
2026-09-22 18:06:24 +0300

    main5.py

---
+++ main5.py
@@ -0,0 +1,9 @@
+n1=int(input( "Number of the students in class 1: "))
+n2=int(input( "Number of the students in class 2: "))
+n3=int(input( "Number of the students in class 3: "))
+
+a=(n1+1)//2
+b=(n2+1)//2
+c=(n3+1)//2
+
+print("Number of desks: ", a+b+c )
