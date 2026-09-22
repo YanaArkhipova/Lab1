f255d52db07266d4757111dbf42f5b114895578b
YanaArkhipova <yanaarkhipova1511@gmail.com>
2026-09-22 17:58:29 +0300

    main.py

---
+++ main.py
@@ -0,0 +1,7 @@
+number1= float(input(" first number "))
+number2= float(input(" second number "))
+number3= float(input(" third number "))
+
+sum= print(number1+number2+number3)
+product= print(number1*number2*number3)
+power= print(number1**(number2-number3))
