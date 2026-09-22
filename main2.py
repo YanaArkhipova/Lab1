ae880cff0b4e2d367821666f56403a5a7c7dac17
YanaArkhipova <yanaarkhipova1511@gmail.com>
2026-09-22 18:04:34 +0300

    main2.py

---
+++ main2.py
@@ -0,0 +1,7 @@
+n1=int(input("distance in meters 1: "))
+n2=int(input("distance in meters 2: "))
+n3=int(input("distance in meters 3: "))
+
+print( "kilometers:", n1//1000)
+print( "kilometers:", n2//1000)
+print( "kilometers:", n3//1000)
